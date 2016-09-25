89876044e30303c4468471d7dfab2f2f7402528a
Ranip Hore <johnykrrish@gmail.com>
2016-09-25 14:57:25 +0530

    Initial evaluation done of project4 assignments.

--- Projects/04/fill/Fill.asm
+++ Projects/04/fill/Fill.asm
@@ -9,3 +9,77 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(INFINITELOOP)
+@KBD
+D = M
+
+@KEYNOTPRESSED
+D;JEQ
+
+@8192
+D = A
+@n
+M = D //n=8192
+@i
+M = 1     // i=1
+
+@SCREEN   //16384
+D = A
+@addr
+M = D  // addr = SCREEN = 16384
+
+(LOOP)
+@addr
+A = M
+M = -1
+@addr
+M = M + 1  //addr = addr+1
+@i
+M = M + 1  //i=i+1
+@i
+D = M
+@n
+D = D - M  //i = i - n
+@INFINITELOOP
+D;JGT
+@LOOP
+0;JMP
+
+@INFINITELOOP
+D;JMP
+
+(KEYNOTPRESSED)
+@i
+M = 1     // i=1
+
+@SCREEN   //16384
+D = A
+@addr
+M = D  // addr = SCREEN = 16384
+
+(LOOP1)
+@addr
+A = M
+M = 0
+@addr
+M = M + 1  //addr = addr+1
+@i
+M = M + 1  //i=i+1
+@i
+D = M
+@n
+D = D - M  //i = i - n
+@INFINITELOOP
+D;JGT
+@LOOP1
+0;JMP
+
+@INFINITELOOP
+D;JMP
+
+@INFINITELOOP
+D;JMP
+
+
+
--- Projects/04/mult/Mult.asm
+++ Projects/04/mult/Mult.asm
@@ -7,3 +7,56 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R0
+D = M
+@A
+M = D	// A = R0
+
+@R1
+D = M
+@B
+M = D	// B = R1
+
+@R2
+M = 0	// Initialize product to zero
+
+//We will add A to itself B times, to calculate A*B.
+//	i = 0,sum = 0;
+//	while(i < B)
+//  {
+//		sum = sum + A;
+//		i++;
+//	}
+//	product = sum;
+
+@i
+M = 0  //	i = 0
+@sum
+M = 0  //	sum = 0
+
+(LOOP)
+@B
+D = M
+@i
+D = M - D	//	D = i - B
+@LOOPEND
+D;JEQ		
+@A
+D = M
+@sum
+M = M + D	//	sum = sum + A
+@i
+M = M + 1	//	i++
+@LOOP
+0;JMP
+
+(LOOPEND)
+@sum
+D = M
+@R2
+M = D
+
+(END)
+@END
+0;JMP
